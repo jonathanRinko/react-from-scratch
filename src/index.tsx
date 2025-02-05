e950acd7ec29edc9292ab863b8b219bde354cab8
unknown <jon.j.rinko@gmail.com>
2025-02-04 21:42:10 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(<div>This is a new React app</div>);
